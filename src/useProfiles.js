53285f560b9921bf0f1fa992aa1e69c4dbd61e1d
Simon Arnold <simonh.arnold@gmail.com>
2022-03-19 10:36:59 +0100

    Profiles send 1-3 generic responses and then a final configurable response

--- src/useProfiles.js
+++ src/useProfiles.js
@@ -1,20 +1,6 @@
 import { useState } from "react";
-
+import { shuffle } from "./helpers";
 import ALL_PROFILES from "./profiles";
-
-/**
- * Found at https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
- * Thanks, Jeff!
- * Shuffles array in place. ES6 version
- * @param {Array} a items An array containing the items.
- */
-function shuffle(a) {
-    for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
-    }
-    return a;
-}
 
 function shuffleProfiles() {
     return shuffle([...ALL_PROFILES]);
